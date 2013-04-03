e83f482888b70c12eb153b3c94e78cbacfc744f1
Paul Forti <paul@itsbi.fr>
2013-04-04 01:16:50 +0200

    fixed typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:ryanb/#{application}.git"
+set :repository, "git@github.com:kustom666/#{application}.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
