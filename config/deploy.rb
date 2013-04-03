2c596a06bbf58a01b4aadef8376732f0d4c63f88
Paul <Forti>
2013-04-03 18:12:37 +0200

    prepared for live deploy

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+require "bundler/capistrano"
+
+set :application, "Antenna"
+set :repository,  "git@github.com:kustom666/antenna.git"
+
+set :scm, "git"
+set :user, "deploy"  # The server's user for deploys
+set :scm_passphrase, "p@ssw0rd"  # The deploy user's password
+
+set :branch, "master"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "lookingfordev.com"                          # Your HTTP server, Apache/etc
+role :app, "lookingfordev.com"                          # This may be the same as your `Web` server
+role :db,  "lookingfordev.com", :primary => true # This is where Rails migrations will run
+
+
+set :deploy_via, :remote_cache
+
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
