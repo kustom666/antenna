2490599cfd35310543f48bea04e35250c07aebb7
Paul Forti <paul@itsbi.fr>
2013-04-04 19:38:59 +0200

    added a max host in deplo

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
-namespace :deploy do
+namespace :deploy, :max_hosts => 4 do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
