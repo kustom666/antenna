e29738385a86e26d160438935fe4e56dcfe06160
Paul Forti <paul@itsbi.fr>
2013-04-15 23:00:38 +0200

    corrected several video bugs

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,5 +3,5 @@ class Video < ActiveRecord::Base
 	belongs_to :playlist
   attr_accessible :name, :playlist_id, :number
   validates_presence_of :name, :playlist_id, :number
-  validates_uniqueness_of :number
+  validates_uniqueness_of :number, :scope => :playlist_id
 end
